edc331ee52379be8eba5b3e0d49b70175d84c13c
Dylan Cross <dylnnzl@gmail.com>
2017-02-27 19:55:57 +1300

    DB stub functions

---
+++ database.js
@@ -0,0 +1,97 @@
+/**
+ * Created by Dylan on 27-Feb-17.
+ */
+// Modules
+var knex = require('knex');
+// Files
+
+//      ********************
+//      * Insert Functions *
+//      ********************
+
+/**
+ * Insert outfit into outfit database
+ */
+function outfitInsert() {
+
+}
+
+/**
+ * Insert facility into outfitFacility database
+ */
+function outfitFacilityInsert() {
+
+}
+
+/**
+ * Insert facility into playerFacility database
+ */
+function playerFacilityInsert() {
+
+}
+
+/**
+ * Insert xp event into xp database
+ */
+function xpInsert() {
+
+}
+
+/**
+ * Insert character into tracked database
+ */
+function trackedInsert() {
+
+}
+
+/**
+ * Insert kill/death event into deaths database
+ */
+function deathsInsert() {
+
+}
+
+//      ******************
+//      * Read Functions *
+//      ******************
+/**
+ * Read outfit from outfit database
+ */
+function outfitRead() {
+
+}
+
+/**
+ * Read facility from outfitFacility database
+ */
+function outfitFacilityRead() {
+
+}
+
+/**
+ * Read facility from playerFacility database
+ */
+function playerFacilityRead() {
+
+}
+
+/**
+ * Read xp event from xp database
+ */
+function xpRead() {
+
+}
+
+/**
+ * Read character from tracked database
+ */
+function trackedRead() {
+
+}
+
+/**
+ * Read kill/death event from deaths database
+ */
+function deathsRead() {
+
+}
