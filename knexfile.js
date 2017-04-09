96fde681a92be9e9a92316919c74884f5968031e
Dylan Cross <dylnnzl@gmail.com>
2017-04-09 16:02:38 +1200

    Corrected db path name

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
         client: 'sqlite3',
         connection: {
-            filename: '../dev.db'
+            filename: __dirname + '/dev.db'
         }
     },
 
