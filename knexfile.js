8aff130e4e6e4e13e3ffe3aa783f4c8b61262dd6
Dylan Cross <dylnnzl@gmail.com>
2017-04-24 19:36:58 +1200

    Added back environment variables

--- knexfile.js
+++ knexfile.js
@@ -29,10 +29,10 @@ module.exports = {
     production: {
         client: 'postgresql',
         connection: {
-            host :    '104.236.130.152',
-            database: 'test1',
-            user:     'test1',
-            password: 'test1'
+            host :    process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
+            user:     process.env.DB_USER,
+            password: process.env.DB_PASS
         },
         pool: {
             min: 2,
